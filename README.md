03c4385ebc67e9897210421fade43a2f675d6e6e
Michael Pell <michaelcpell@gmail.com>
2014-07-26 15:30:28 -0600

    Things work

--- README.md
+++ README.md
@@ -1,20 +1,45 @@
-#run_analysis.R Explanation
+##run_analysis.R Explanation
 
 The script is broken into 4 main steps.  
 
-## 1. Data Aggregation
+### 1. Data Aggregation
 This step merges the train and test datasets.  There were 3 main dimensions of data to merge, and I describe them as first being merged "vertically", and then "horizontally" concatenated.
 
 Vertical merge means that I used the rbind() function to put the train/test datasets together of like kind: I rbind()'d subject_id data from train and test to create a combined dataset, etc..
 
 After I had vertically rbind()'d two dimensions (subject and activity), I could then concatenate them  to make a data.frame with two columns.  
 
-## 2. Data Translation
+### 2. Data Translation
 The remaining dimension was the column of vectors of measurements.  After combining test and train, I used statistical summarizations for mean and st dev to summarize the the values into two additional columns, mean and stdev.
 
-## 3. Data Reshaping
+### 3. Data Reshaping
 I believe there was something like 30 readings for each combination of subject_id and activity_id, for each variable, so the goal was to summarize each of those 30 readings into 1 using a mean().
 This was accomplished by melting and casting using the 'reshape' library.
 
-## 4. Data Outputting
+### 4. Data Outputting
 I used write.table without any line numbers to output a tidy dataset to .txt.
+
+##Original Dataset
+
+==================================================================
+Human Activity Recognition Using Smartphones Dataset
+Version 1.0
+==================================================================
+Jorge L. Reyes-Ortiz, Davide Anguita, Alessandro Ghio, Luca Oneto.
+Smartlab - Non Linear Complex Systems Laboratory
+DITEN - Università degli Studi di Genova.
+Via Opera Pia 11A, I-16145, Genoa, Italy.
+activityrecognition@smartlab.ws
+www.smartlab.ws
+==================================================================
+
+
+##
+The new dataset includes the following files:
+=========================================
+run_analysis.R - A file that converts the files of the original dataset to a tidy dataset.
+reshaped_tidy_data.txt - A space-seperated table with headers.  It captures the 30 day averages of the daily averages of tri-axial movement of test subjects recorded via Samsung Galaxy 2 smartphone.  The data is categorized by subject_id and sub-categorized by activity_id.  It is output by run_analysis.R.
+
+sum_features.txt = a file with a list of the summary features that are to be maintained.
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -30,9 +30,12 @@
   data2 = read.table("~/Desktop/UCI HAR Dataset/train/X_train.txt")
   data3 = rbind(data1,data2)
 
-#Data Translation
-  data_table_1$mean = rowMeans(data3)
-  data_table_1$stdev = apply(data3,1,sd)
+  keep_cols = c("V1","V2","V3","V4","V5","V6","V41","V42","V43","V44","V45","V46","V81","V82","V83","V84","V85","V86","V121","V122","V123","V124","V125","V126","V161","V162","V163","V164","V165","V166","V201","V202","V214","V215","V227","V228","V240","V241","V253","V254","V266","V267","V268","V269","V270","V271","V345","V346","V347","V348","V349","V350","V424","V425","V426","V427","V428","V429","V503","V504","V516","V517","V529","V530","V542","V543")
+  data4 = data3[keep_cols]
+
+  data5 = rename(data4,c("V1" = "tBodyAcc-mean()-X","V2" = "tBodyAcc-mean()-Y","V3" = "tBodyAcc-mean()-Z","V4" = "tBodyAcc-std()-X","V5" = "tBodyAcc-std()-Y","V6" = "tBodyAcc-std()-Z","V41" = "tGravityAcc-mean()-X","V42" = "tGravityAcc-mean()-Y","V43" = "tGravityAcc-mean()-Z","V44" = "tGravityAcc-std()-X","V45" = "tGravityAcc-std()-Y","V46" = "tGravityAcc-std()-Z","V81" = "tBodyAccJerk-mean()-X","V82" = "tBodyAccJerk-mean()-Y","V83" = "tBodyAccJerk-mean()-Z","V84" = "tBodyAccJerk-std()-X","V85" = "tBodyAccJerk-std()-Y","V86" = "tBodyAccJerk-std()-Z","V121" = "tBodyGyro-mean()-X","V122" = "tBodyGyro-mean()-Y","V123" = "tBodyGyro-mean()-Z","V124" = "tBodyGyro-std()-X","V125" = "tBodyGyro-std()-Y","V126" = "tBodyGyro-std()-Z","V161" = "tBodyGyroJerk-mean()-X","V162" = "tBodyGyroJerk-mean()-Y","V163" = "tBodyGyroJerk-mean()-Z","V164" = "tBodyGyroJerk-std()-X","V165" = "tBodyGyroJerk-std()-Y","V166" = "tBodyGyroJerk-std()-Z","V201" = "tBodyAccMag-mean()","V202" = "tBodyAccMag-std()","V214" = "tGravityAccMag-mean()","V215" = "tGravityAccMag-std()","V227" = "tBodyAccJerkMag-mean()","V228" = "tBodyAccJerkMag-std()","V240" = "tBodyGyroMag-mean()","V241" = "tBodyGyroMag-std()","V253" = "tBodyGyroJerkMag-mean()","V254" = "tBodyGyroJerkMag-std()","V266" = "fBodyAcc-mean()-X","V267" = "fBodyAcc-mean()-Y","V268" = "fBodyAcc-mean()-Z","V269" = "fBodyAcc-std()-X","V270" = "fBodyAcc-std()-Y","V271" = "fBodyAcc-std()-Z","V345" = "fBodyAccJerk-mean()-X","V346" = "fBodyAccJerk-mean()-Y","V347" = "fBodyAccJerk-mean()-Z","V348" = "fBodyAccJerk-std()-X","V349" = "fBodyAccJerk-std()-Y","V350" = "fBodyAccJerk-std()-Z","V424" = "fBodyGyro-mean()-X","V425" = "fBodyGyro-mean()-Y","V426" = "fBodyGyro-mean()-Z","V427" = "fBodyGyro-std()-X","V428" = "fBodyGyro-std()-Y","V429" = "fBodyGyro-std()-Z","V503" = "fBodyAccMag-mean()","V504" = "fBodyAccMag-std()","V516" = "fBodyBodyAccJerkMag-mean()","V517" = "fBodyBodyAccJerkMag-std()","V529" = "fBodyBodyGyroMag-mean()","V530" = "fBodyBodyGyroMag-std()","V542" = "fBodyBodyGyroJerkMag-mean()","V543" = "fBodyBodyGyroJerkMag-std()"))
+
+data_table_1 = cbind(data_table_1,data5)
 
 #Data Reshaping
   library(reshape)
